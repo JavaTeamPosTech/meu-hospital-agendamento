46d9524527520ef0f00db852e256c2755ddd9878
Rafael Caxixi <rafael.fuzeti@sptech.school>
2025-09-30 20:08:30 -0300

    Serviço conectando e se comunicando com serviço de notificação

--- src/main/java/com/postechfiap/meu_hospital_agendamento/core/controllers/consultas/CadastrarConsultaInputPort.java
+++ src/main/java/com/postechfiap/meu_hospital_agendamento/core/controllers/consultas/CadastrarConsultaInputPort.java
@@ -3,5 +3,5 @@ package com.postechfiap.meu_hospital_agendamento.core.controllers.consultas;
 import com.postechfiap.meu_hospital_agendamento.core.dtos.consulta.CadastrarConsultaInputModel;
 
 public interface CadastrarConsultaInputPort {
-    void execute(CadastrarConsultaInputModel input);
+    void execute(CadastrarConsultaInputModel input, String emailPaciente);
 }
